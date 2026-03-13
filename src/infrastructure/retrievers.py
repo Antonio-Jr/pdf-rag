18a91ea9748311e3bdbe6c9fce3dcab658444211
Antonio-Jr <antonio.jr.ssouza@gmail.com>
2026-03-13 17:47:23 -0300

    feat: Adding retrievers.py for vector db. This retrievers.py considers the user_id to avoid users' query documents that are not owned by them

---
+++ src/infrastructure/retrievers.py
@@ -0,0 +1,27 @@
+from langchain_postgres.vectorstores import PGVector
+
+from src.utils.log_wrapper import log_execution
+from src.core.settings import settings
+from src.utils.embedding_factory import get_embeddings
+
+
+@log_execution
+def get_retriever(user_id: str, k: int = 5, additional_filters: dict | None = None):
+    embeddings = get_embeddings()
+    vector_store = PGVector(
+        connection=settings.DATABASE_URL,
+        collection_name=settings.DATABASE_COLLECTION_NAME,
+        embeddings=embeddings,
+        create_extension=False,
+        use_jsonb=True,
+        async_mode=True,
+    )
+
+    compose_filter = {"user_id": user_id}
+
+    if additional_filters:
+        compose_filter.update(additional_filters)
+
+    search_kwargs = {"k": k, "filter": compose_filter}
+
+    return vector_store.as_retriever(search_kwargs=search_kwargs)
